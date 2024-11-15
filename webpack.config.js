184ca33a140032fb31ba1892655784ffc7168d98
DegaGit <deganaqualy@gmail.com>
2024-11-15 12:50:04 -0300

    Adicionando arquivos iniciais

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+// webpack.config.js
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+};
